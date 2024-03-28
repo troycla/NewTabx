5e12e9acb99203d5d9bcf494852ddad4985fd02a
Admin x <69062572+troycla@users.noreply.github.com>
2024-03-28 17:57:20 -0500

    Update uv.config.js

--- g/proxy-a/u-v/uv/uv.config.js
+++ g/proxy-a/u-v/uv/uv.config.js
@@ -1,9 +1,10 @@
 self.__uv$config = {
-    bare: '/bare/',
     prefix: '/service/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/uv.handler.js',
-    bundle: '/uv.bundle.js',
-    config: '/uv.config.js',
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
