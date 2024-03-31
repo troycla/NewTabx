b1dcb48a585136e5013c4669743f76438d084b86
Admin x <69062572+troycla@users.noreply.github.com>
2024-03-31 13:39:04 -0500

    Add files via upload

---
+++ g/app-a/chat-gpt/config.js
@@ -0,0 +1,5 @@
+const API_URL = "https://api.chatanywhere.com.cn/v1/chat/completions";
+const strIndex = "sk-d1jZqWYoLTbKLX8zr0c2I5an1LkYEOhju16ubxW5EY1FgVmt";
+const system_message = "You are a large language model (LLM) called ChatGPT. All queries must be in English."; // You can change this message
+const path = "";  // Change path to your path. For localhost make it path = "";
+// const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
