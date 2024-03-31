815954502b6407d66e0e708ed8d3aacb7f2982e2
Admin x <69062572+troycla@users.noreply.github.com>
2024-03-31 13:58:24 -0500

    Update apps.js

--- g/js/apps.js
+++ g/js/apps.js
@@ -2,5 +2,5 @@
     window.location.href = ('app-a/d-p1/index.html');
   }
   function chat() {
-    window.location.href = ('app-a/chat-gpt/index.html')
+    window.location.href = ('app-a/chat-gpt/index.html');
   }
