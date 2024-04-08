dab41be24a95141f29f9427720a9b8c20e78611e
Admin x <69062572+troycla@users.noreply.github.com>
2024-04-08 17:25:40 -0500

    Update config.js

--- g/app-a/chat-gpt/config.js
+++ g/app-a/chat-gpt/config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.chatanywhere.com.cn/v1/chat/completions";
-const strIndex = "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";
+const strIndex = "sk-d1jZqWYoLTbKLX8zr0c2I5an1LkYEOhju16ubxW5EY1FgVmt";
 const system_message = "You are a large language model (LLM) called ChatGPT. All queries must be in English."; // You can change this message
 const path = "";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
